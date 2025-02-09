6dc16595de83cc8b7693a3c11ed02471939682ac
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-02-09 21:08:43 +0530

    init

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++20")
+set_policy("build.sanitizer.address", true)
+set_policy("build.sanitizer.undefined", true)
+add_rules("plugin.compile_commands.autoupdate")
+add_cxxflags("-Wall -Wextra -Wpedantic -Wshadow -g")
+
+for _, file in ipairs(os.files("src/*.cpp")) do
+	local name = path.basename(file)
+
+	target(name)
+		set_kind("binary")
+		add_files(file)
+end
