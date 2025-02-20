ecb87fa922538929a7822e54febecf42e0447e8a
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-02-21 00:00:40 +0530

    add dbg-macro

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,7 @@ set_policy("build.sanitizer.address", true)
 set_policy("build.sanitizer.undefined", true)
 add_rules("plugin.compile_commands.autoupdate")
 add_cxxflags("-Wall -Wextra -Wpedantic -Wshadow -g")
+add_requires("dbg-macro")
 
 for _, file in ipairs(os.files("src/*.cpp")) do
 	local name = path.basename(file)
@@ -12,4 +13,5 @@ for _, file in ipairs(os.files("src/*.cpp")) do
 	target(name)
 		set_kind("binary")
 		add_files(file)
+    add_packages("dbg-macro")
 end
